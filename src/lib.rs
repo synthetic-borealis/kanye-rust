4e24dcdf5d8f0c414b79c7dca7a8c80954e7c233
synthetic-borealis <snip.coloured@gmail.com>
2022-10-12 19:04:27 +0300

    convert previous code to a shuttle serverless function

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,24 @@
 #[macro_use]
 extern crate rocket;
 
+use crate::quotes::*;
+use rocket::serde::json::Json;
+
+mod quotes;
+
+#[get("/text")]
+fn text() -> &'static str {
+    get_random_quote()
+}
+
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> Json<Quote> {
+    Json(Quote::new())
 }
 
 #[shuttle_service::main]
 async fn rocket() -> shuttle_service::ShuttleRocket {
-    let rocket = rocket::build().mount("/hello", routes![index]);
+    let rocket = rocket::build().mount("/", routes![index, text]);
 
     Ok(rocket)
 }
